eb43fbf924f3b4a6612d7a3f20fb4b2f6f360386
imstrong <koolhagie@gmail.com>
2022-06-17 15:33:04 +0900

    first commit

---
+++ app/src/main/java/com/sky/camera/CameraView.kt
@@ -0,0 +1,206 @@
+package com.sky.camera
+
+import android.content.Context
+import android.net.Uri
+import android.util.Log
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.sharp.Lens
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.*
+import io.ktor.client.request.*
+import io.ktor.client.request.forms.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import kotlinx.coroutines.*
+
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.Executor
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+@Composable
+fun CameraView(
+    outputDirectory: File,
+    executor: Executor,
+    onImageCaptured: (Uri) -> Unit,
+    onError: (ImageCaptureException) -> Unit
+) {
+
+    // 1
+    val lensFacing = CameraSelector.LENS_FACING_BACK
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    val preview = Preview.Builder().build()
+    val previewView = remember { PreviewView(context) }
+    val imageCapture: ImageCapture = remember { ImageCapture.Builder().build() }
+    val cameraSelector = CameraSelector.Builder()
+        .requireLensFacing(lensFacing)
+        .build()
+
+    // 2
+    LaunchedEffect(lensFacing) {
+        val cameraProvider = context.getCameraProvider()
+        cameraProvider.unbindAll()
+        cameraProvider.bindToLifecycle(
+            lifecycleOwner,
+            cameraSelector,
+            preview,
+            imageCapture
+        )
+
+        preview.setSurfaceProvider(previewView.surfaceProvider)
+    }
+
+    // 3
+    Box(contentAlignment = Alignment.BottomCenter, modifier = Modifier.fillMaxSize()) {
+        AndroidView({ previewView }, modifier = Modifier.fillMaxSize())
+
+        IconButton(
+            modifier = Modifier.padding(bottom = 20.dp),
+            onClick = {
+                Log.i("kilo", "ON CLICK")
+                takePhoto(
+                    context = context,
+                    filenameFormat = "yyyy-MM-dd-HH-mm-ss-SSS",
+                    imageCapture = imageCapture,
+                    outputDirectory = outputDirectory,
+                    executor = executor,
+                    onImageCaptured = onImageCaptured,
+                    onError = onError
+                )
+
+            },
+            content = {
+                Icon(
+                    imageVector = Icons.Sharp.Lens,
+                    contentDescription = "Take picture",
+                    tint = Color.White,
+                    modifier = Modifier
+                        .size(100.dp)
+                        .padding(1.dp)
+                        .border(1.dp, Color.White, CircleShape)
+                )
+            }
+        )
+    }
+}
+
+private fun takePhoto(
+    context: Context,
+    filenameFormat: String,
+    imageCapture: ImageCapture,
+    outputDirectory: File,
+    executor: Executor,
+    onImageCaptured: (Uri) -> Unit,
+    onError: (ImageCaptureException) -> Unit
+) {
+    val photoFile = File(
+        outputDirectory,
+        SimpleDateFormat(filenameFormat, Locale.US).format(System.currentTimeMillis()) + ".jpg"
+    )
+
+    val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+    imageCapture.takePicture(outputOptions, executor, object: ImageCapture.OnImageSavedCallback {
+        override fun onError(exception: ImageCaptureException) {
+            Log.e("kilo", "Take photo error:", exception)
+            onError(exception)
+        }
+
+        override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+            val savedUri = Uri.fromFile(photoFile)
+
+            onImageCaptured(savedUri)
+
+
+            val inputStream = context.contentResolver.openInputStream(savedUri)
+            val imageByteArray = inputStream?.readBytes()
+
+            // val scope = CoroutineScope(Job() + Dispatchers.IO)
+//            Log.e("kilo","imageByteArray size: ${imageByteArray?.size}")
+            val scope = CoroutineScope(Dispatchers.IO)
+            scope.launch {
+                uploadImage("ABCD123",savedUri, imageByteArray)
+            }
+        }
+    })
+}
+
+private suspend fun Context.getCameraProvider(): ProcessCameraProvider = suspendCoroutine { continuation ->
+    ProcessCameraProvider.getInstance(this).also { cameraProvider ->
+        cameraProvider.addListener({
+            continuation.resume(cameraProvider.get())
+        }, ContextCompat.getMainExecutor(this))
+    }
+}
+
+
+//suspend fun uploadImage(text: String, byteArray: ByteArray?): Boolean {
+suspend fun uploadImage(text: String,fileUri:Uri,byteArray: ByteArray?): Boolean {
+    return try {
+        if (byteArray != null) {
+
+            val client = HttpClient(CIO)
+
+            val response: HttpResponse = client.submitFormWithBinaryData(
+                url = "http://haihuozhan.com:8083/process_upload.php",
+                formData = formData {
+                    //the line below is just an example of how to send other parameters in the same request
+                    append("hbl", text)
+                    append("file", byteArray, Headers.build {
+                        append(HttpHeaders.ContentType, "image/jpeg")
+                        append(HttpHeaders.ContentDisposition, "filename=${fileUri.lastPathSegment}")
+                    })
+                }
+            )
+            {
+                onUpload { bytesSentTotal, contentLength ->
+                    println("Sent $bytesSentTotal bytes from $contentLength")
+                }
+            }
+
+            response.also {
+                println(it.status)
+            }
+//            println(response.toString())
+            println(fileUri.lastPathSegment)
+
+        }
+        true
+    } catch (ex: Exception) {
+//im using timber for logs, you can always replace this with Log.d
+        Log.d("TAG","error ${ex.message}")
+        false
+    }
+}
